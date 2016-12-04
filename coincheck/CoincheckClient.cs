f1abe4a69fa960898790ac154a4bb6c2fee9f1bf
mmatthew <takeknock@gmail.com>
2016-12-04 18:35:22 +0900

    initial commit

---
+++ coincheck/CoincheckClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+
+namespace Coincheck
+{ 
+    public class CoincheckClient
+    {
+        private HttpClient client;
+        private string target;
+        CoincheckClient()
+        {
+            client = new HttpClient();
+            target = "https://coincheck.com";
+        }
+        
+    }
+}
