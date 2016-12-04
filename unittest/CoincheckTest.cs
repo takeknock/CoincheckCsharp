1e25d5fea6cd8728620f179d357507c112bf3489
mmatthew <takeknock@gmail.com>
2016-12-05 01:07:56 +0900

    added unittest forCoincheckClient.getTicker()

--- unittest/CoincheckTest.cs
+++ unittest/CoincheckTest.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NUnit.Framework;
+using Coincheck;
 
 namespace unittest
 {
+    [TestFixture]
     class CoincheckTest
     {
+        [Test]
+        public void testGetTicker()
+        {
+            var client = new CoincheckClient();
+            Ticker ticker = client.getTicker();
+            Assert.AreNotEqual(ticker, null);
+            ticker.serialize();
+        }
 
     }
 }
--- unittest/Program.cs
+++ unittest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Coincheck;
 
 namespace Unittest
 {
@@ -10,7 +11,6 @@ namespace Unittest
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
