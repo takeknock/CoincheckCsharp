66357847aa5e14ca17393527200e66dd209dc7b1
mmatthew <takeknock@gmail.com>
2016-12-10 19:30:49 +0900

    added UnixTime class for calculating nonce

---
+++ coincheck/UnixTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coincheck
+{
+    public class UnixTime
+    {
+        private readonly DateTime UNIXEPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public long _value { get; private set; }
+
+        public UnixTime(DateTime dateTime)
+        {
+            _value = (long)(dateTime.ToUniversalTime() - UNIXEPOCH).TotalSeconds;
+        }
+    }
+}
