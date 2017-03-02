97fa52b16cbc1e02f934b5e2a5379211df46a68c
mmatthew <takeknock@gmail.com>
2017-03-03 01:02:06 +0900

    Added MockSender class for unit teset

---
+++ unittest/MockSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace unittest
+{
+    class MockSender : Coincheck.ISender
+    {
+        public string _key { get; private set; }
+        public string _secret { get; private set; }
+        public HttpClient _http;
+        public string _method;
+        public Dictionary<string, string> _param = new Dictionary<string, string>();
+
+        public async Task<string> SendAsync(HttpClient http, Uri path, string apiKey, string secret, string method, Dictionary<string, string> parameters = null)
+        {
+            this._key = apiKey;
+            this._secret = secret;
+            this._http = http;
+            this._method = method;
+            this._param = parameters;
+
+            return "success";
+        }
+
+    }
+}
