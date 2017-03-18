8eb4e0ec86985eaa536c201bac4ac95afbf2d0e3
mmatthew <takeknock@gmail.com>
2017-03-18 14:56:47 +0900

    http method classes inherited IHttpMethod

--- coincheck/Util/DELETE.cs
+++ coincheck/Util/DELETE.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace coincheck.Util
 {
-    class DELETE
+    class DELETE : IHttpMethod
     {
+        public async Task<string> sendAsync(HttpClient http, Uri path)
+        {
+            return "hoget";
+        }
     }
 }
--- coincheck/Util/GET.cs
+++ coincheck/Util/GET.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace coincheck.Util
 {
-    class GET
+    class GET : IHttpMethod
     {
+        public async Task<string> sendAsync(HttpClient http, Uri path)
+        {
+            return "fuga";
+        }
     }
 }
