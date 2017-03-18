650e319bb34b6c6a49d9d9863f1dbe37a85b74cf
mmatthew <takeknock@gmail.com>
2017-03-18 14:55:13 +0900

    Added interface of http method classes.

---
+++ coincheck/Util/IHttpMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace coincheck.Util
+{
+    interface IHttpMethod
+    {
+        Task<string> sendAsync(HttpClient http, Uri uri);
+    }
+}
