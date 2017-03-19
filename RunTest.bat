9c2bc2be8104fc3b10f4cfaa3f27047630493b8d
mmatthew <takeknock@gmail.com>
2017-03-19 17:22:07 +0900

    Change absolute path to relative path

--- RunTest.bat
+++ RunTest.bat
@@ -1,3 +1,3 @@
-C:\Users\mmatt\01_work\coincheck\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console C:\Users\mmatt\01_work\coincheck\unittest\bin\Debug\unittest.exe
+.\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console .\unittest\bin\Debug\unittest.exe
 
 pause
